5693898463b31c3736e3ef9b4b8e3626f5d9560b
kirillPressF <prohorenko25@gmail.com>
2024-03-16 12:32:33 +0200

    Create initial structure (foldres, layer). Implement Domain layer

---
+++ CleanArchitectureV2.0.Domain/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureV2._0.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureV2._0.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
