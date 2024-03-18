7ac6be886589a2b9d756f649858d7062b840004c
kirillPressF <prohorenko25@gmail.com>
2024-03-18 22:36:28 +0200

    Create Repositories in core folder App layer

---
+++ CleanArchitectureV2.0.Application/Repositories/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitectureV2._0.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureV2._0.Application.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T> GetById(Guid id, CancellationToken cancellationToken);
+        Task<List<T>> GetAll (CancellationToken cancellationToken);
+    }
+}
---
+++ CleanArchitectureV2.0.Application/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitectureV2._0.Domain.Entities;
+
+namespace CleanArchitectureV2._0.Application.Repositories
+{
+    public interface IUserRepository : IBaseRepository<UserEntity>
+    {
+        Task<UserEntity> GetByEmail(string email, CancellationToken cancellationToken);
+    }
+}
